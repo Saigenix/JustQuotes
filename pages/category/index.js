609a313e54f31a10bc7e63ecf9f2a352f13edb08
saigenix <sainathmahindrakar@gmail.com>
2022-12-20 20:14:11 +0530

    catagory design

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,70 +3,65 @@ import Image from "next/image";
 import styles from "../../styles/Category.module.css";
 
 function Category() {
-  const labels = [
-    "business",
-    "change",
-    "character",
-    "competition",
-    "conservative",
-    "courage",
-    "education",
-    "faith",
-    "family",
-    "famous-quotes",
-    "film",
-    "freedom",
-    "friendship",
-    "future",
-    "happiness",
-    "history",
-    "honor",
-    "humor",
-    "humorous",
-    "inspirational",
-    "leadership",
-    "life",
-    "literature",
-    "love",
-    "motivational",
-    "nature",
-    "pain",
-    "philosophy",
-    "politics",
-    "power-quotes",
-    "proverb",
-    "religion",
-    "science",
-    "self",
-    "self-help",
-    "social-justice",
-    "spirituality",
-    "sports",
-    "success",
-    "technology",
-    "time",
-    "truth",
-    "virtue",
-    "war",
-    "wisdom",
-  ];
+  const labels = ['business',
+  'change',
+  'character',
+  'competition',
+  'conservative',
+  'courage',
+  'education',
+  'faith',
+  'family',
+  'famous-quotes',
+  'film',
+  'freedom',
+  'friendship',
+  'future',
+  'happiness',
+  'history',
+  'honor',
+  'humor',
+  'humorous',
+  'inspirational',
+  'leadership',
+  'life',
+  'literature',
+  'love',
+  'motivational',
+  'nature',
+  'pain',
+  'philosophy',
+  'politics',
+  'power-quotes',
+  'religion',
+  'science',
+  'self',
+  'self-help',
+  'social-justice',
+  'spirituality',
+  'sports',
+  'success',
+  'technology',
+  'time',
+  'truth',
+  'war',
+  'wisdom']
+  
   return (
     <div className={styles.box}>
       {
         labels.map(val => {
           return(
-            <>
+            <div key={val} className={styles.smallBox}>
             <Link href={`/category/${val}`}>
-            <a className={styles.cat}>{val}</a>
+            <a className={styles.btn}>{val}</a>
             </Link>
-            </>
+            </div>
+
+          
           )
         })
       }
-      
-      {/* <Link href="/category/love">
-        <a className={styles.cat}>Love</a>
-      </Link> */}
     </div>
   );
 }
