3ed0273365919e7b343ad9b18c2756f58aff33f7
saigenix <sainathmahindrakar@gmail.com>
2023-03-17 21:41:33 +0530

    17-3/1

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,51 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../../styles/Category.module.css";
+import { labels } from "../../constants/constant";
+
 
 function Category() {
-  const labels = ['business',
-  'change',
-  'character',
-  'competition',
-  'conservative',
-  'courage',
-  'education',
-  'faith',
-  'family',
-  'famous-quotes',
-  'film',
-  'freedom',
-  'friendship',
-  'future',
-  'happiness',
-  'history',
-  'honor',
-  'humor',
-  'humorous',
-  'inspirational',
-  'leadership',
-  'life',
-  'literature',
-  'love',
-  'motivational',
-  'nature',
-  'pain',
-  'philosophy',
-  'politics',
-  'power-quotes',
-  'religion',
-  'science',
-  'self',
-  'self-help',
-  'social-justice',
-  'spirituality',
-  'sports',
-  'success',
-  'technology',
-  'time',
-  'truth',
-  'war',
-  'wisdom']
+  
   
   return (
     <div className={styles.box}>
